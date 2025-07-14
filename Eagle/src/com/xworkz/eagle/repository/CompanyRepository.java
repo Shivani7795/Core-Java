9caa4bb0e516ec94588fea758d22c2eb5140510c
Shivani <shivani332433@gmail.com>
2025-07-14 13:26:46 +0530

    set and sorting

--- Eagle/src/com/xworkz/eagle/repository/CompanyRepository.java
+++ Eagle/src/com/xworkz/eagle/repository/CompanyRepository.java
@@ -2,8 +2,11 @@ package com.xworkz.eagle.repository;
 
 import com.xworkz.eagle.dto.CompanyDto;
 
+import java.util.List;
 import java.util.Set;
 
 public interface CompanyRepository {
     Set<CompanyDto> findCompany();
+
+    List<CompanyDto> findCompanySortedByNameDesc();
 }
